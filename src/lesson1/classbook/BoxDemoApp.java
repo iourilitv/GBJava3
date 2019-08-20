06eaaa197bf3391830a83834aaef00361747aa28
iourilitv <iourilitv1965@gmail.com>
2019-08-20 12:55:41 +0300

    Completed the examples.

--- src/lesson1/classbook/BoxDemoApp.java
+++ src/lesson1/classbook/BoxDemoApp.java
@@ -1,6 +1,8 @@
 package lesson1.classbook;
 
 /**
+ * GBJava3
+ * Java Core. Profesional level. 19.08.2019 Webinar. Teacher: Anton Batiaev
  * Урок 1. Обобщения - Generics.
  * Понятие обобщения. Обобщенные классы, методы и
  * интерфейсы. Наследование обобщенных классов. Ограничения
@@ -9,10 +11,19 @@ package lesson1.classbook;
  */
 public class BoxDemoApp {
     public static void main(String[] args) {
+
+        //создаем две коробки intBox1 и intBox2, в которые при
+        //инициализации складываем значения 20 и 30
         SimpleBox intBox1 = new SimpleBox( 20 );
         SimpleBox intBox2 = new SimpleBox( 30 );
+
+        //проверяем, что изъятый объект является экземпляром(instanceof) класса
+        //целочисленных значений, чтобы не получить исключение ClassCastException
+        // при сложении обектов.
         if (intBox1.getObj() instanceof Integer && intBox2.getObj() instanceof
                 Integer) {
+
+            //приводим объекты к целочисленному значению и суммируем
             int sum = (Integer)intBox1.getObj() + (Integer)intBox2.getObj();
             System.out.println( "sum = " + sum);
         } else {
--- src/lesson1/classbook/SimpleBox.java
+++ src/lesson1/classbook/SimpleBox.java
@@ -1,6 +1,8 @@
 package lesson1.classbook;
 
 /**
+ * GBJava3
+ * Java Core. Profesional level. 19.08.2019 Webinar. Teacher: Anton Batiaev
  * Урок 1. Обобщения - Generics.
  * Понятие обобщения. Обобщенные классы, методы и
  * интерфейсы. Наследование обобщенных классов. Ограничения
@@ -8,14 +10,20 @@ package lesson1.classbook;
  * Пример без обобщений.ERR ClassCastException.
  */
 
+//задача создать класс, который позволит хранить в себе один объект любого типа.
 public class SimpleBox {
+    //единственное поле типа Object
     private Object obj;
+
+    //
     public Object getObj() {
         return obj;
     }
+
     public void setObj(Object obj) {
         this.obj = obj;
     }
+
     public SimpleBox(Object obj) {
         this.obj = obj;
     }
