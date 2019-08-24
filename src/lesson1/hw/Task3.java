cb7b7ab3526b4bdf1bfbd34ddd199d130236094c
iourilitv <iourilitv1965@gmail.com>
2019-08-24 11:11:33 +0300

    Copied the code of Homework1 from the teacher.

--- src/lesson1/hw/Task3.java
+++ src/lesson1/hw/Task3.java
@@ -2,7 +2,7 @@ package lesson1.hw;
 
 /**
  * GBJava3
- * Java Core. Profesional level. 19.08.2019 Webinar. Teacher: Anton Batiaev
+ * Java Core. Professional level. 19.08.2019 Webinar. Teacher: Anton Batiaev
  * Урок 1. Обобщения - Generics.
  * Домашняя работа.
  * @author Litvinenko Yuriy
