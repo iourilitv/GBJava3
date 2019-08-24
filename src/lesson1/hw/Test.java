fa2e40ddeee014901655f312a91b9d45916d0ef2
iourilitv <iourilitv1965@gmail.com>
2019-08-24 10:46:38 +0300

    Just added some examples on class name getting.

--- src/lesson1/hw/Test.java
+++ src/lesson1/hw/Test.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Test {
 
     public static void main(String[] args) {
+        Fruit apple = new Apple(1.0f);
         Store store = new Store();
         //инициализируем коллекцию коробок
         store.boxes = new ArrayList<>();
@@ -13,11 +14,19 @@ public class Test {
         store.createBoxes(5);
 
         System.out.println("store.boxes.get(0).getClass().getTypeName(): " + store.boxes.get(0).getClass().getTypeName());
-        //System.out.println("store.boxes.get(0).getClass().getTypeParameters(): " + store.boxes.get(0).getClass().getTypeParameters().toString());
+        System.out.println("store.boxes.get(0).getClass().getSimpleName(): " + store.boxes.get(0).getClass().getSimpleName());
+        System.out.println("store.boxes.get(0).getClass().getTypeParameters().getClass().getSimpleName(): " + store.boxes.get(0).getClass().getTypeParameters().getClass().getSimpleName());
+        System.out.println("apple.getClass().getSimpleName(): " + apple.getClass().getSimpleName());
+        System.out.println("store.boxes.get(0).getUnits().get(0).getClass().getSimpleName(): " + store.boxes.get(0).getUnits().get(0).getClass().getSimpleName());
 
         //Result.
         //store.boxes.get(0).getClass().getTypeName(): lesson1.hw.Box
         //store.boxes.get(0).getClass().getTypeParameters(): [Ljava.lang.reflect.TypeVariable;@1b6d3586
+        //store.boxes.get(0).getClass().getSimpleName(): Box
+        //store.boxes.get(0).getClass().getTypeParameters().getClass().getSimpleName(): TypeVariable[]
+        //apple.getClass().getSimpleName(): Apple
+        //store.boxes.get(0).getUnits().get(0).getClass().getSimpleName():
+        // Exception in thread "main" java.lang.IndexOutOfBoundsException: Index: 0, Size: 0
 
         //TODO
         // Так и не нашел, как у обекта new Box<Apple>("Apple", 10)) в коллекции boxes в классе Store
