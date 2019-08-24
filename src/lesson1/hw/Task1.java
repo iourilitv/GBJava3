4188343bec52b190ff9f306a80c96cde76a93563
iourilitv <iourilitv1965@gmail.com>
2019-08-24 11:12:30 +0300

    Just make a grammar correction. Copied the code of Homework1 from the teacher.

--- src/lesson1/hw/Task1.java
+++ src/lesson1/hw/Task1.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 /**
  * GBJava3
- * Java Core. Profesional level. 19.08.2019 Webinar. Teacher: Anton Batiaev
+ * Java Core. Professional level. 19.08.2019 Webinar. Teacher: Anton Batiaev
  * Урок 1. Обобщения - Generics.
  * Домашняя работа.
  * @author Litvinenko Yuriy
