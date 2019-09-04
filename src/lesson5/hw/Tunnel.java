c098decad11ef9957ff9171257b2eaabcb32b1df
iourilitv <iourilitv1965@gmail.com>
2019-09-04 13:24:12 +0300

    The hw tasks have been completed. Completed the task of semaphore in tunnel. Completed the task of adding a barrier at the start line and have broke ExecutorService shutdown. Completed the ExecutorService shutdown after all threads finished.

--- src/lesson5/hw/Tunnel.java
+++ src/lesson5/hw/Tunnel.java
@@ -1,7 +1,12 @@
 package lesson5.hw;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
-    //FIXME поставить Semaphore tunnel.
+    //TODO HW.Adding Semaphore in tunnel.Added
+    //создаем объект семафор с заданным лимитом доступа в параметре
+    Semaphore semaphore = new Semaphore( 2 );
+
     public Tunnel() {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
@@ -12,14 +17,28 @@ public class Tunnel extends Stage {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
-                //FIXME поставить Semaphore tunnel. Count + 1
+
+                //TODO HW.Adding Semaphore in tunnel.Added
+                //поток участника(машины) запрашивает у семафора право на ресурс и
+                // ждем пока не дано право прохода(значение счетчика больше 0)
+                semaphore.acquire();
+
                 System.out.println(c.getName() + " начал этап: " + description);
+
+                //TODO HW.Adding Semaphore in tunnel.Added
+                System.out.println("***Свободно мест в тоннеле: " + semaphore.availablePermits());;
+
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
-                //FIXME поставить Semaphore tunnel. Count - 1
+
+                //TODO HW.Adding Semaphore in tunnel.Added
+                //освобождаем тоннель для одного следующего участника
+                // (счетчик семафора +1, но не больше разрешенных permits)
+                semaphore.release();
+
             }
         } catch (Exception e) {
             e.printStackTrace();
