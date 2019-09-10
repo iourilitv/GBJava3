0b8fe450ed5a768504d96871d697e4b50f7bd931
iourilitv <iourilitv1965@gmail.com>
2019-09-10 09:08:12 +0300

    Copied the class files of lesson 7 from the teacher.

---
+++ src/main/java/lesson7/BankAccount.java
@@ -0,0 +1,24 @@
+package lesson7;
+
+/**
+ * BankAccount
+ *
+ * @author anton
+ * @since 09/09/19
+ */
+@Table(name = "bank_account")
+public final class BankAccount extends Account {//Account перечеркнут, т.к. класс помечен @Deprecated(устаревший)
+    private String cardId;
+
+    public BankAccount(String id) {
+        super(id);
+    }
+
+    public String getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(String cardId) {
+        this.cardId = cardId;
+    }
+}
