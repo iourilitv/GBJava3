5e3a19b3a390f0ec2996c430f26eb494dbdba6da
iourilitv <iourilitv1965@gmail.com>
2019-09-11 12:12:42 +0300

    Added the files of tutorial from http://tutorials.jenkov.com/java-reflection/annotations.html. It does not work.

---
+++ src/main/java/lesson7/hw/tutorial/TheClass.java
@@ -0,0 +1,11 @@
+package lesson7.hw.tutorial;
+
+public class TheClass {
+
+
+
+
+    /*public static void doSomethingElse(
+            @MyAnnotation(name="aName", value="aValue") String parameter){
+    }*/
+}
