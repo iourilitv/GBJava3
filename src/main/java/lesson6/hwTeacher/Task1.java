09e41182ef7e7e94bde36c4b7ab08508e2d8576b
iourilitv <iourilitv1965@gmail.com>
2019-09-24 14:27:09 +0300

    Just added a comment.

--- src/main/java/lesson6/hwTeacher/Task1.java
+++ src/main/java/lesson6/hwTeacher/Task1.java
@@ -1,11 +1,20 @@
 package lesson6.hwTeacher;
 
+import java.util.Arrays;
+
+/**
+ * Похоже, эта реализация расчитана на узкую задачу.
+ */
 public class Task1 {
     private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(Task1.class);
 
     public static void main(String[] args) {
-        final int[] res2 = process(new int[]{1, 2, 3, 4, 5, 6, 7, 8});
-        System.out.println(res2);
+        int[] initArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+        //final int[] res2 = process(new int[]{1, 2, 3, 4, 5, 6, 7, 8});
+        final int[] res2 = process(initArray);
+        //System.out.println(res2);
+        System.out.println(Arrays.toString(res2));
+        System.out.println(Arrays.toString(initArray));
     }
 
     public static int[] process(int[] arrIn) {
@@ -14,7 +23,7 @@ public class Task1 {
             log.info("В массиве нет элементов");
             return arrIn;
         }
-        for (int i = arrIn.length - 3; i >= 0; i--) {////TODO ? Почему - 3?
+        for (int i = arrIn.length - 3; i >= 0; i--) {//TODO ? Почему - 3?
             if (arrIn[i] == 4) {
                 System.arraycopy(arrIn, (i + 1), backArr, 0, 2);
                 return backArr;
